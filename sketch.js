97de7aa8b50b7cc9e2e446e192814003bd0ea7f1
saisamarth1234 <63238298+saisamarth1234@users.noreply.github.com>
2020-04-08 18:45:43 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,98 @@
+var fixedRect, movingRect;
+var gameObject1, gameObject2, gameObject3, gameObject4;
+
+function setup() {
+  createCanvas(1200,800);
+  fixedRect = createSprite(600, 400, 50, 80);
+  fixedRect.shapeColor = "green";
+ 
+  movingRect = createSprite(800, 400,80,30);
+  movingRect.shapeColor = "green";
+
+  gameObject1 = createSprite(100, 100, 50, 50);
+  gameObject1.shapeColor = "green";
+  gameObject2 = createSprite(200, 100, 50, 50);
+  gameObject2.shapeColor = "green";
+  gameObject3 = createSprite(300, 100, 50, 50);
+  gameObject3.shapeColor = "green";
+  gameObject4 = createSprite(400, 100, 50, 50);
+  gameObject4.shapeColor = "green";
+}
+
+function draw() {
+  background(0,0,0);  
+  movingRect.x = World.mouseX;
+  movingRect.y = World.mouseY;
+
+  if(isTouching(movingRect, gameObject1)){
+    movingRect.shapeColor = "blue";
+    gameObject1.shapeColor = "blue";
+  }
+  else {
+    movingRect.shapeColor = "green";
+    gameObject1.shapeColor = "green";
+  }
+  if(isTouching(movingRect, gameObject2)){
+    movingRect.shapeColor = "yellow";
+    gameObject2.shapeColor = "yellow";
+  }
+  else {
+    movingRect.shapeColor = "green";
+    gameObject2.shapeColor = "green";
+  }
+  if(isTouching(movingRect, gameObject3)){
+    movingRect.shapeColor = "magenta";
+    gameObject3.shapeColor = "magenta";
+  }
+  else {
+    movingRect.shapeColor = "green";
+    gameObject3.shapeColor = "green";
+  }
+  if(isTouching(movingRect, gameObject4)){
+    movingRect.shapeColor = "cyan";
+    gameObject4.shapeColor = "cyan";
+  }
+  else {
+    movingRect.shapeColor = "green";
+    gameObject4.shapeColor = "green";
+  }
+  if(isTouching(movingRect, fixedRect)){
+    movingRect.shapeColor = "red";
+    fixedRect.shapeColor = "red";
+  }
+  else {
+    movingRect.shapeColor = "green";
+    fixedRect.shapeColor = "green";
+  }
+  drawSprites();
+  
+  bounceOff(movingRect,fixedRect);
+  drawSprites();
+}
+
+function bounceOff(object1,object2){
+  if (object1.x - object2.x < object2.width/2 + object1.width/2
+    && object2.x - object1.x < object2.width/2 + object1.width/2) {
+    object1.velocityX = object1.velocityX * (-1);
+    object2.velocityX = object2.velocityX * (-1);
+  }
+  if (object1.y - object2.y < object2.height/2 + object1.height/2
+    && object2.y - object2.y < object2.height/2 + object1.height/2) {
+      object1.velocityY = object1.velocityY * (-1);
+      object2.velocityY = object2.velocityY * (-1);
+  } 
+}
+}
+
+function isTouching(object1,object2){
+  if (object1.x - object2.x < object2.width/2 + object1.width/2
+    && object2.x - object1.x < object2.width/2 + object1.width/2
+    && object1.y - object2.y < object2.height/2 + object1.height/2
+    && object2.y - object2.y < object2.height/2 + object1.height/2) {
+    
+    return true;
+  }
+  else {
+    return false;
+  } 
+}
